143e732acba63df8635942642ddbebb108cb8795
Khalil <khalilferjaoui@yahoo.fr>
2026-02-16 16:44:25 +0100

    mx(policy): Add cudastream as std::uintptr_t for GCC compatibility

--- common/00_mx/include/mx/utils/policy.h
+++ common/00_mx/include/mx/utils/policy.h
@@ -8,10 +8,11 @@ struct CPU {
 };
 
 struct CUDA {
-        std::uint32_t block = 256;
-        std::uint32_t grid_x = 2048;
-        std::uint32_t grid_y = 1;
-        std::uint32_t grid_z = 1;
+    std::uint32_t block = 256;
+    std::uint32_t grid_x = 2048;
+    std::uint32_t grid_y = 1;
+    std::uint32_t grid_z = 1;
+    std::uintptr_t stream = 0; // 0 == default stream
 };
 
 }
