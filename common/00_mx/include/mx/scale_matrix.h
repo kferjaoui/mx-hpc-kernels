2b1161a3f8c7df83e758a3e42421d50878937252
kferjaoui <khalilferjaoui@yahoo.fr>
2026-03-22 13:14:09 +0100

    fix matrix scaling

--- common/00_mx/include/mx/scale_matrix.h
+++ common/00_mx/include/mx/scale_matrix.h
@@ -6,14 +6,14 @@
 namespace mx{
 
 template<typename T, class Layout = RowMajor>
-void scale_matrix(const T beta, Dense<T,Layout> C){
+void scale_matrix(const T beta, Dense<T,Layout>& C){
     scale_matrix(beta, C.view());
-
 }
+
 template<typename T, class Layout = RowMajor>
 void scale_matrix(const T beta, DenseView<T,Layout> C){
     const index_t N = C.rows();
-    const index_t M = C.rows();
+    const index_t M = C.cols();
     if constexpr (C.is_row_major){
         if (beta == T{0}) {
             for (index_t i = 0; i < N; ++i)
