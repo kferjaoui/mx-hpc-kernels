1de319f357dffba79b5849ca7967167fc3b8654a
Khalil <khalilferjaoui@yahoo.fr>
2026-04-01 13:51:38 +0200

    mx(cuda/gemm): collapse K-tile loop into SharedMemTiling kernel
    - reduce kernel launch overhead.
    - eliminate the need for loading C_tile into shared memory.
    - scale C_tile once before writing back to global memory.

--- 04_gemm/include/mx_gemm/detail/device/gemm_shmem_tiling.cuh
+++ 04_gemm/include/mx_gemm/detail/device/gemm_shmem_tiling.cuh
@@ -9,8 +9,7 @@ template <typename T>
 __global__ void gemm_shmem_tile_partial(const T alpha, const T* __restrict__ A, const T* __restrict__ B,
                                     const T beta, T* __restrict__ C,
                                     const size_t N, const size_t M, const size_t K,
-                                    const size_t N_TILE, const size_t M_TILE, const size_t K_TILE,
-                                    int K_TILE_START) 
+                                    const size_t N_TILE, const size_t M_TILE, const size_t K_TILE) 
 {
     // Calculate local thread ID
     auto TILE_SIZE = N_TILE * M_TILE;
@@ -25,54 +24,46 @@ __global__ void gemm_shmem_tile_partial(const T alpha, const T* __restrict__ A, 
 
     auto size_A_tile = N_TILE * K_TILE;
 
-    // load A_tile in the first (N_Tile * K_TILE) elements in row-major order
-    auto offset_A_tile = K_TILE_START + K * (N_TILE * blockIdx.y);
-    for(size_t idx = 0; idx < size_A_tile; idx += TILE_SIZE){
-        auto col_shift = (local_tid_1d + idx) % K_TILE;
-        auto row_shift = (local_tid_1d + idx) / K_TILE;
+    T C_ij{0};
 
-        if ( (local_tid_1d + idx) < size_A_tile ) shmem[local_tid_1d + idx] = A[offset_A_tile + col_shift + K * row_shift];
-    }
+    for(int start_K_TILE=0; start_K_TILE<K; start_K_TILE += K_TILE){
+        auto actual_K_TILE = std::min(K-start_K_TILE, K_TILE);
 
-    auto size_B_tile = K_TILE*M_TILE;
-    auto start_B_tile = size_A_tile;
-    auto end_B_tile = size_A_tile + size_B_tile;
+        // load A_tile in the first (N_Tile * K_TILE) elements in row-major order
+        auto offset_A_tile = start_K_TILE + K * (N_TILE * blockIdx.y);
+        for(size_t idx = 0; idx < size_A_tile; idx += TILE_SIZE){
+            auto col_shift = (local_tid_1d + idx) % actual_K_TILE;
+            auto row_shift = (local_tid_1d + idx) / actual_K_TILE;
+
+            if ( (local_tid_1d + idx) < size_A_tile ) shmem[local_tid_1d + idx] = A[offset_A_tile + col_shift + K * row_shift];
+        }
+
+        auto size_B_tile = actual_K_TILE*M_TILE;
+        auto start_B_tile = size_A_tile;
+        auto end_B_tile = size_A_tile + size_B_tile;
+        
+        // load B_tile in the next (K_Tile * M_TILE) elements in row-major order
+        auto offset_B_tile = (M_TILE * blockIdx.x) + start_K_TILE * M;
+        for(size_t idx = 0; idx < size_B_tile; idx += TILE_SIZE){
+            auto col_shift = (local_tid_1d + idx) % M_TILE;
+            auto row_shift = (local_tid_1d + idx) / M_TILE;
+            if ( (local_tid_1d + idx) < size_B_tile  ) shmem[start_B_tile + local_tid_1d + idx] = B[offset_B_tile + col_shift + M * row_shift];
+        }
+
+        __syncthreads(); // make sure the tiles are loaded before we start computing
     
-    // load B_tile in the next (K_Tile * M_TILE) elements in row-major order
-    auto offset_B_tile = (M_TILE * blockIdx.x) + K_TILE_START * M;
-    for(size_t idx = 0; idx < size_B_tile; idx += TILE_SIZE){
-        auto col_shift = (local_tid_1d + idx) % M_TILE;
-        auto row_shift = (local_tid_1d + idx) / M_TILE;
-        if ( (local_tid_1d + idx) < size_B_tile  ) shmem[start_B_tile + local_tid_1d + idx] = B[offset_B_tile + col_shift + M * row_shift];
-    }
+        for(size_t k=0; k < actual_K_TILE; ++k){
+            auto local_start_row_i_of_A = threadIdx.y * actual_K_TILE;
+            auto local_start_col_j_of_B = threadIdx.x;
+            // access contiguous elements in row of A_tile but strided elements in col of B_tile
+            C_ij += alpha * shmem[local_start_row_i_of_A + k] * shmem[start_B_tile + local_start_col_j_of_B + k * M_TILE]; 
+        }
 
-    // load C_tile in the next (N_TILE * M_TILE) elements
-    auto size_C_tile = N_TILE * M_TILE; 
-    auto end_C_tile = end_B_tile + size_C_tile;
-    
-    if (gtid_x < M && gtid_y < N) { // guard againt OOB access of C
-        shmem[end_B_tile + local_tid_1d] = C[gtid_y * M + gtid_x]; // each thread loads one element of C_tile
-    }  
-
-    __syncthreads(); // make sure the tiles are loaded before we start computing
-  
-    // first K_TILE, we need to scale C by beta
-    T C_ij = shmem[end_B_tile + local_tid_1d];
-    if (K_TILE_START == 0) {
-        // we know there is exactly one element of C_tile per thread (no need to loop OR check bounds) -> TODO: generalize?
-        C_ij *= beta;
+        __syncthreads(); // wait for all threads to finish before overwriting the shared memory with the next tile
     }
-
-    for(size_t k=0; k < K_TILE; ++k){
-        auto local_start_row_i_of_A = threadIdx.y * K_TILE;
-        auto local_start_col_j_of_B = threadIdx.x;
-        // access contiguous elements in row of A_tile but strided elements in col of B_tile
-        C_ij += alpha * shmem[local_start_row_i_of_A + k] * shmem[start_B_tile + local_start_col_j_of_B + k * M_TILE]; 
-    }
-    __syncthreads();
 
     if (gtid_x < M && gtid_y < N) { // guard againt OOB access of C
-        C[gtid_y * M + gtid_x] = C_ij;
+        C[gtid_y * M + gtid_x] = beta * C[gtid_y * M + gtid_x] + C_ij;
     }      
 }
 
@@ -90,13 +81,10 @@ __host__ void gemm_shmem_tiled(const T alpha, const T* __restrict__ dA, const T*
     auto grid_y = (N + N_TILE -1) / N_TILE;
     dim3 grid(grid_x, grid_y, 1);
     dim3 threadBlock(M_TILE, N_TILE, 1);
-    const size_t shmemBytes = sizeof(T) * ( N_TILE * M_TILE + K_TILE * (N_TILE + M_TILE) );
+    const size_t shmemBytes = sizeof(T) * ( K_TILE * (N_TILE + M_TILE) );
 
-    for(int start_K_TILE=0; start_K_TILE<K; start_K_TILE += K_TILE){
-        auto actual_K_TILE = std::min(K-start_K_TILE, K_TILE);
-        gemm_shmem_tile_partial<<<grid, threadBlock, shmemBytes, stream>>>(
-                                    alpha, dA, dB, beta, dC, N, M, K, N_TILE, M_TILE, actual_K_TILE, start_K_TILE);
-    }
+    gemm_shmem_tile_partial<<<grid, threadBlock, shmemBytes, stream>>>(
+                                alpha, dA, dB, beta, dC, N, M, K, N_TILE, M_TILE, K_TILE);
 }
 
 } // namespace mx::detail
